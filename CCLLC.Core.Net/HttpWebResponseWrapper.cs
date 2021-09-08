1113b8ffe957adaf97b2550887bc94896b4ff04b
Scott Colson <scott@colsoncode.com>
2021-09-08 05:36:12 -0700

    Update WebResponse to include both byte[] and string content.

--- CCLLC.Core.Net/HttpWebResponseWrapper.cs
+++ CCLLC.Core.Net/HttpWebResponseWrapper.cs
@@ -1,11 +1,15 @@
-﻿using System.IO;
-using System.Net;
+﻿
 
 namespace CCLLC.Core.Net
-{    
+{
+    using System.IO;
+    using System.Net;
+    using System.Text;
+
     public class HttpWebResponseWrapper : IWebResponse
     {
         public string Content { get; }
+        public byte[] Bytes { get; }
         public WebHeaderCollection Headers { get;  }
         public int StatusCode { get;  }
         public string StatusDescription { get;  }
@@ -25,10 +29,16 @@ namespace CCLLC.Core.Net
                 this.StatusDescription = response.StatusDescription;
                 this.Success = (this.StatusCode >= 200 && this.StatusCode < 300);
 
-                using (StreamReader content = new StreamReader(response.GetResponseStream()))
+               
+                using (var stream = response.GetResponseStream())
                 {
-                    this.Content = content.ReadToEnd();
-                }
+                    var ms = new MemoryStream();
+                    stream.CopyTo(ms);
+                    var bytes = ms.ToArray();
+                    this.Bytes = bytes;
+                    this.Content = new UTF8Encoding().GetString(bytes);
+                }               
+
             }
         }
     }
--- CCLLC.Core.Net/Interfaces/IWebResponse.cs
+++ CCLLC.Core.Net/Interfaces/IWebResponse.cs
@@ -5,6 +5,7 @@ namespace CCLLC.Core.Net
     public interface IWebResponse
     {
         string Content { get; }
+        byte[] Bytes { get; }
         WebHeaderCollection Headers { get; }
         int StatusCode { get; }
         string StatusDescription { get; }
